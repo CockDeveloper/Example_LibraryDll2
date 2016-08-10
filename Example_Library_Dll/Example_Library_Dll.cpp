baa5777ade5414a0374b04b67db005edf88a85fc
CockDeveloper <COCK.Developer@gmail.com>
2016-08-10 07:56:25 +0700

    Define main to call methods in library

--- Example_Library_Dll/Example_Library_Dll.cpp
+++ Example_Library_Dll/Example_Library_Dll.cpp
@@ -1,11 +1,25 @@
 // Example_Library_Dll.cpp : Defines the entry point for the console application.
+// Compile by using: cl /EHsc /link MathLibrary.lib MathClient.cpp
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include "MathLibrary.h"
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+    double a = 7.4;
+    int b = 99;
+
+    cout << "a + b = " <<
+        MathLibrary::Functions::Add(a, b) << endl;
+    cout << "a * b = " <<
+        MathLibrary::Functions::Multiply(a, b) << endl;
+    cout << "a + (a * b) = " <<
+        MathLibrary::Functions::AddMultiply(a, b) << endl;
+
+    return 0;
 }
 
